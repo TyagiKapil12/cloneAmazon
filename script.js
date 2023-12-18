7deac2aee0528bf7a0f9bfefc5e6b7881f920818
TyagiKapil12 <153435119+TyagiKapil12@users.noreply.github.com>
2023-12-18 16:21:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// let newbutt = document.createElement("button");
+// newbutt.innerText = "click me!";
+// newbutt.style.color = "white";
+// newbutt.style.background = "red";
+
+
+// document.querySelector("body").prepend(newbutt);
+
+
+// let para = 
+
+const array1 = ['G','e','e','k','s'];
+console.log('array1:',array1);
+
+const reversed = array1.reverse();
+console.log('reversed:',reversed);
+
+console.log(array1);
